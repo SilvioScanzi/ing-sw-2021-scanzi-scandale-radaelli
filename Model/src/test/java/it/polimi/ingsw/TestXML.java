e8778195a3d2f58f89fc1ea856a4e789a27c3bda
SilvioScanzi <silvio.scanzi@mail.polimi.it>
2021-03-31 17:04:14 +0200

    Added XML file Parsing for developmentCards

---
+++ Model/src/test/java/it/polimi/ingsw/TestXML.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw;
+import java.io.*;
+
+public class TestXML {
+    public static void main(String[] args){
+        DevelopmentCardMarket DC = new DevelopmentCardMarket();
+        DevelopmentCard D = DC.getFirstCard(Game.Colours.Green,1);
+        System.out.println("livello: " + D.getLevel());
+        if(D.getColour().equals(Game.Colours.Green)) System.out.println("Colore: verde");
+        else System.out.println("Colore: Blu");
+        System.out.println("COSTO:");
+        D.getCost().entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        });
+        System.out.println("REQUISITI:");
+        D.getrequiredResources().entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        });
+        System.out.println("PRODOTTI:");
+        D.getproducedResources().entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        });
+        System.out.println("Fede:" + D.getproducedFaith() + "\n");
+
+        D = DC.getFirstCard(Game.Colours.Green,1);
+        System.out.println("livello: " + D.getLevel());
+        if(D.getColour().equals(Game.Colours.Green)) System.out.println("Colore: verde");
+        else System.out.println("Colore: Blu");
+        System.out.println("COSTO:");
+        D.getCost().entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        });
+        System.out.println("REQUISITI:");
+        D.getrequiredResources().entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        });
+        System.out.println("PRODOTTI:");
+        D.getproducedResources().entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        });
+        System.out.println("Fede:" + D.getproducedFaith());
+    }
+}
