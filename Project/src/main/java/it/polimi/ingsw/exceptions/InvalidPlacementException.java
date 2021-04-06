fac75f2e77e832fe38775acd1385718a2cd87a5f
Silvio Scanzi <79810010+SilvioScanzi@users.noreply.github.com>
2021-04-06 20:30:41 +0200

    Deleted files for future refactor

--- Project/src/main/java/it/polimi/ingsw/exceptions/InvalidPlacementException.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.exceptions;
-
-public class InvalidPlacementException extends Exception{
-    public InvalidPlacementException(String message) {
-        super(message);
-    }
-}
