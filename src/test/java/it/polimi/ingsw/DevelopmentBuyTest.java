3fde1611de4b101c9668a3bf6e53fc4cd79caba9
SilvioScanzi <silvio.scanzi@mail.polimi.it>
2021-04-12 14:38:46 +0200

    Fixed testing on DCCards

--- src/test/java/it/polimi/ingsw/DevelopmentBuyTest.java
+++ src/test/java/it/polimi/ingsw/DevelopmentBuyTest.java
@@ -4,7 +4,6 @@ import it.polimi.ingsw.model.*;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.*;
@@ -36,10 +35,16 @@ public class DevelopmentBuyTest{
         }
         catch(Exception e) {e.printStackTrace();}
 
+        ArrayList<Pair<String,Integer>> choice = new ArrayList<>();
+        choice.add(new Pair<>("SE",3));
+        choice.add(new Pair<>("SE",3));
+        choice.add(new Pair<>("PI",2));
+        choice.add(new Pair<>("PI",2));
+
         try {
-            game.getDevelopmentCard(Colours.Purple, 1, 0, 1);
+            game.getDevelopmentCard(Colours.Purple, 1, 0, 1,choice);
         }catch(Exception e) {e.printStackTrace();}
-        //System.out.println(playerBoard.slottoString());
+
         assert(playerBoard.getSlot(1).getList().size()==1);
         assert(playerBoard.getSlot(2).getList().size()==0);
         assert(playerBoard.getSlot(3).getList().size()==0);
@@ -53,21 +58,42 @@ public class DevelopmentBuyTest{
 
     @Test
     @DisplayName("Ensure correct behaviour of development card buying action when resources are fully got from the strongbox")
-    void testCanBuyDCCardFromSB(){
-        playerBoard.getStrongbox().addResource(Resources.Coins,99);
-        playerBoard.getStrongbox().addResource(Resources.Stones,99);
-        playerBoard.getStrongbox().addResource(Resources.Shields,99);
-        playerBoard.getStrongbox().addResource(Resources.Servants,99);
+    void testCanBuyDCCardFromSB() {
+        playerBoard.getStrongbox().addResource(Resources.Coins, 99);
+        playerBoard.getStrongbox().addResource(Resources.Stones, 99);
+        playerBoard.getStrongbox().addResource(Resources.Shields, 99);
+        playerBoard.getStrongbox().addResource(Resources.Servants, 99);
+        //System.out.println(game.getDevelopmentcardmarket().toString());
+
+
         try{
-            game.getDevelopmentCard(Colours.Purple, 1, 0, 1);
-            game.getDevelopmentCard(Colours.Green, 1, 0, 2);
-            game.getDevelopmentCard(Colours.Blue, 1, 0, 3);
-            game.getDevelopmentCard(Colours.Yellow, 2, 0, 1);
-            game.getDevelopmentCard(Colours.Purple, 2, 0, 2);
-            game.getDevelopmentCard(Colours.Green, 2, 0, 3);
-            game.getDevelopmentCard(Colours.Blue, 3, 0, 1);
-            game.getDevelopmentCard(Colours.Yellow, 3, 0, 2);
-            game.getDevelopmentCard(Colours.Purple, 3, 0, 3);
+            ArrayList<Pair<String,Integer>> choice = new ArrayList<Pair<String,Integer>>()
+                {{add(new Pair<>("SE",6));add(new Pair<>("SE",6));add(new Pair<>("PI",6));add(new Pair<>("PI",6));}};
+            game.getDevelopmentCard(Colours.Purple, 1, 0, 1,choice);
+            choice = new ArrayList<Pair<String,Integer>>()
+            {{add(new Pair<>("MO",6));add(new Pair<>("MO",6));add(new Pair<>("SC",6));add(new Pair<>("SC",6));}};
+            game.getDevelopmentCard(Colours.Green, 1, 0, 2,choice);
+            choice = new ArrayList<Pair<String,Integer>>()
+            {{add(new Pair<>("MO",6));add(new Pair<>("MO",6));add(new Pair<>("SE",6));add(new Pair<>("SE",6));}};
+            game.getDevelopmentCard(Colours.Blue, 1, 0, 3,choice);
+            choice = new ArrayList<Pair<String,Integer>>()
+            {{add(new Pair<>("SE",6));add(new Pair<>("PI",6));add(new Pair<>("SE",6));add(new Pair<>("PI",6));add(new Pair<>("SE",6));add(new Pair<>("PI",6));}};
+            game.getDevelopmentCard(Colours.Yellow, 2, 0, 1,choice);
+            choice = new ArrayList<Pair<String,Integer>>()
+            {{add(new Pair<>("SE",6));add(new Pair<>("SC",6));add(new Pair<>("SE",6));add(new Pair<>("SC",6));add(new Pair<>("SE",6));add(new Pair<>("SC",6));}};
+            game.getDevelopmentCard(Colours.Purple, 2, 0, 2,choice);
+            choice = new ArrayList<Pair<String,Integer>>()
+            {{add(new Pair<>("MO",6));add(new Pair<>("SC",6));add(new Pair<>("SC",6));add(new Pair<>("SC",6));add(new Pair<>("MO",6));add(new Pair<>("MO",6));}};
+            game.getDevelopmentCard(Colours.Green, 2, 0, 3,choice);
+            choice = new ArrayList<Pair<String,Integer>>()
+            {{add(new Pair<>("MO",6));add(new Pair<>("MO",6));add(new Pair<>("PI",6));add(new Pair<>("PI",6));add(new Pair<>("MO",6));add(new Pair<>("MO",6));add(new Pair<>("PI",6));add(new Pair<>("PI",6));}};
+            game.getDevelopmentCard(Colours.Blue, 3, 0, 1,choice);
+            choice = new ArrayList<Pair<String,Integer>>()
+            {{add(new Pair<>("SE",6));add(new Pair<>("SE",6));add(new Pair<>("PI",6));add(new Pair<>("PI",6));add(new Pair<>("SE",6));add(new Pair<>("SE",6));add(new Pair<>("PI",6));add(new Pair<>("PI",6));}};
+            game.getDevelopmentCard(Colours.Yellow, 3, 0, 2,choice);
+            choice = new ArrayList<Pair<String,Integer>>()
+            {{add(new Pair<>("SE",6));add(new Pair<>("SE",6));add(new Pair<>("SC",6));add(new Pair<>("SC",6));add(new Pair<>("SE",6));add(new Pair<>("SE",6));add(new Pair<>("SC",6));add(new Pair<>("SC",6));}};
+            game.getDevelopmentCard(Colours.Purple, 3, 0, 3,choice);
         }catch(Exception e) {e.printStackTrace();}
         //System.out.println(playerBoard.slottoString());
         assert(playerBoard.getSlot(1).getList().size()==3);
@@ -85,8 +111,10 @@ public class DevelopmentBuyTest{
         catch(Exception e) {e.printStackTrace();}
         playerBoard.getStrongbox().addResource(Resources.Servants,1);
         playerBoard.getStrongbox().addResource(Resources.Stones,1);
+        ArrayList<Pair<String,Integer>> choice = new ArrayList<Pair<String,Integer>>()
+        {{add(new Pair<>("SE",6));add(new Pair<>("SE",1));add(new Pair<>("PI",2));add(new Pair<>("PI",6));}};
         try{
-            game.getDevelopmentCard(Colours.Purple, 1, 0, 1);
+            game.getDevelopmentCard(Colours.Purple, 1, 0, 1,choice);
         }catch(Exception e) {e.printStackTrace();}
         //System.out.println(playerBoard.slottoString());
         assert(playerBoard.getSlot(1).getList().size()==1);
@@ -97,29 +125,35 @@ public class DevelopmentBuyTest{
     @Test
     @DisplayName("Ensure correct behaviour of development card buying action when a lack of resources impedes the action")
     void testCannotBuyDCCardResources() {
-        assertThrows(IllegalArgumentException.class,()->{game.getDevelopmentCard(Colours.Purple, 1, 0, 1);});
+        ArrayList<Pair<String,Integer>> choice = new ArrayList<Pair<String,Integer>>()
+        {{add(new Pair<>("SE",6));add(new Pair<>("SE",6));add(new Pair<>("PI",6));add(new Pair<>("PI",6));}};
+        assertThrows(IllegalArgumentException.class,()->{game.getDevelopmentCard(Colours.Purple, 1, 0, 1,choice);});
     }
 
     @ParameterizedTest
     @MethodSource("provideCombinationsOfColoursAndLevel")
     @DisplayName("Ensure correct behaviour of development card buying action when slot choice impedes the action")
-    void testCannotBuyDCCardSlot(Colours c, int l, int s, Colours c1, int l1, int s1){
+    void testCannotBuyDCCardSlot(Colours c, int l, int s,ArrayList<Pair<String,Integer>>choice){
         playerBoard.getStrongbox().addResource(Resources.Coins,99);
         playerBoard.getStrongbox().addResource(Resources.Stones,99);
         playerBoard.getStrongbox().addResource(Resources.Shields,99);
         playerBoard.getStrongbox().addResource(Resources.Servants,99);
         try {
-            game.getDevelopmentCard(c, l, 0, s);
+            game.getDevelopmentCard(Colours.Purple,1,0,1,new ArrayList<Pair<String,Integer>>(){{add(new Pair<>("SE",6));add(new Pair<>("SE",6));add(new Pair<>("PI",6));add(new Pair<>("PI",6));}});
         }catch(Exception e){e.printStackTrace();}
-        assertThrows(Exception.class,()->{game.getDevelopmentCard(c1, l1, 0, s1);});
+        assertThrows(Exception.class,()->{game.getDevelopmentCard(c, l, 0, s,choice);});
     }
 
     private static Stream<Arguments> provideCombinationsOfColoursAndLevel() {
         return Stream.of(
-                Arguments.of(Colours.Purple,1,1,Colours.Green,3,1),
-                Arguments.of(Colours.Blue,1,1,Colours.Yellow,2,2),
-                Arguments.of(Colours.Yellow,1,1,Colours.Purple,1,1),
-                Arguments.of(Colours.Blue,1,1,Colours.Green,3,2)
+                Arguments.of(Colours.Green,3,1,new ArrayList<Pair<String,Integer>>()
+                    {{add(new Pair<>("MO",6));add(new Pair<>("SC",6));add(new Pair<>("SC",6));add(new Pair<>("MO",6));add(new Pair<>("MO",6));add(new Pair<>("SC",6));add(new Pair<>("SC",6));add(new Pair<>("MO",6));}}),
+                Arguments.of(Colours.Yellow,2,2,new ArrayList<Pair<String,Integer>>()
+                    {{add(new Pair<>("PI",6));add(new Pair<>("SE",6));add(new Pair<>("PI",6));add(new Pair<>("SE",6));add(new Pair<>("SE",6));add(new Pair<>("PI",6));}}),
+                Arguments.of(Colours.Purple,1,1,new ArrayList<Pair<String,Integer>>()
+                    {{add(new Pair<>("SE",6));add(new Pair<>("PI",6));add(new Pair<>("PI",6));add(new Pair<>("SE",6));}}),
+                Arguments.of(Colours.Green,3,2,new ArrayList<Pair<String,Integer>>()
+                {{add(new Pair<>("MO",6));add(new Pair<>("SC",6));add(new Pair<>("SC",6));add(new Pair<>("MO",6));add(new Pair<>("MO",6));add(new Pair<>("SC",6));add(new Pair<>("SC",6));add(new Pair<>("MO",6));}})
         );
     }
 }
